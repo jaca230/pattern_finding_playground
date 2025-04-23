083e125167457cc1832b5be9e7e7b572ea14ac03
jaca230 <jaca230@uky.edu>
2025-04-23 15:42:55 -0400

    added ability to specify a truth file to teh tracklet reconstruction stage

--- algorithms/tracklet/reconstructed_tracklet_former.py
+++ algorithms/tracklet/reconstructed_tracklet_former.py
@@ -1,4 +1,4 @@
-from typing import List, Any
+from typing import List, Any, Tuple, Optional
 from collections import Counter
 from models.tracklet import Tracklet
 from models.hit import Hit
@@ -6,74 +6,82 @@ from algorithms.tracklet.tracklet_former import TrackletFormer
 from models.point_3d import Point3D
 
 class ReconstructedTrackletFormer(TrackletFormer):
-    def form_tracklets(self, tree: Any, geoHelper: Any, entry_index: int) -> List[Tracklet]:
+    def __init__(self, truth_tree: Optional[Any] = None):
+        self.truth_tree = truth_tree  # Can be None if truth info is unavailable
+
+    def form_tracklets(self, tree: Any, geoHelper: Any, entry_index: int) -> Tuple[List[Tracklet], dict]:
         tree.GetEntry(entry_index)
+        if self.truth_tree:
+            self.truth_tree.GetEntry(entry_index)
 
         tracklets = []
         tracklet_counter = 0
+
+        for index, tracklet in enumerate(tree.trackletVec):
+            particle_id = tracklet.GetPID()
+            e_id = tracklet.GetEID()
+            hits = []
+
+            for hit in tracklet.GetAllHits():
+                vid = hit.GetVID()
+                vname = geoHelper.GetVolumeName(vid).Data()
+
+                if 'atar' not in vname:
+                    continue
+
+                detector_side = None
+                if len(vname) > 11:
+                    if vname[11] == 'f':
+                        detector_side = 'front'
+                    elif vname[11] == 'b':
+                        detector_side = 'back'
+
+                hit_obj = Hit(
+                    z=geoHelper.GetZ(vid) + 0.07,
+                    x=geoHelper.GetX(vid),
+                    y=geoHelper.GetY(vid),
+                    time=hit.GetObservedTime(),
+                    energy=hit.GetObservedEdep(),
+                    particle_id=hit.GetPID(),
+                    detector_side=detector_side
+                )
+                hits.append(hit_obj)
+
+            # Convert ROOT::Math::XYZVector to Point3D
+            start_point = tracklet.GetStartPoint()
+            stop_point = tracklet.GetStopPoint()
+            point_0 = Point3D(start_point.X(), start_point.Y(), start_point.Z())
+            point_1 = Point3D(stop_point.X(), stop_point.Y(), stop_point.Z())
+
+            t = Tracklet(
+                tracklet_id=tracklet_counter,
+                particle_id=particle_id,
+                e_id=e_id,
+                hits=hits
+            )
+            t.set_endpoints(point_0, point_1)
+            tracklets.append(t)
+            tracklet_counter += 1
+
+        # Determine which tree to use for truth info
+        truth_source = self.truth_tree if self.truth_tree is not None else tree
+
+        # Extract truth info
         n_patterns_truth = 0
         particles_counter = Counter()
         patterns_truth = {}
 
-        for pattern_idx, pattern in enumerate(tree.patternVec):
+        for pattern_idx, pattern in enumerate(truth_source.patternVec):
             n_patterns_truth += 1
             indices = pattern.GetTrackletIndices()
             pattern_tracklet_ids = []
 
             for index in indices:
-                tracklet = tree.trackletVec[index]
+                tracklet = truth_source.trackletVec[index]
                 particle_id = tracklet.GetPID()
-                e_id = tracklet.GetEID()
-                hits = []
 
-                # Count the particle
                 particles_counter[particle_id] += 1
-
-                # Collect hits
-                for hit in tracklet.GetAllHits():
-                    vid = hit.GetVID()
-                    vname = geoHelper.GetVolumeName(vid).Data()
-
-                    if 'atar' not in vname:
-                        continue
-
-                    detector_side = None
-                    if len(vname) > 11:
-                        if vname[11] == 'f':
-                            detector_side = 'front'
-                        elif vname[11] == 'b':
-                            detector_side = 'back'
-
-                    hit_obj = Hit(
-                        z=geoHelper.GetZ(vid) + 0.07,
-                        x=geoHelper.GetX(vid),
-                        y=geoHelper.GetY(vid),
-                        time=hit.GetObservedTime(),
-                        energy=hit.GetObservedEdep(),
-                        particle_id=hit.GetPID(),
-                        detector_side=detector_side
-                    )
-                    hits.append(hit_obj)
-
-                # Assign tracklet_id and append to pattern map
-                tracklet_id = tracklet_counter
-                tracklet_counter += 1
-                pattern_tracklet_ids.append(tracklet_id)
-
-                # Convert ROOT::Math::XYZVector to Point3D
-                start_point = tracklet.GetStartPoint()
-                stop_point = tracklet.GetStopPoint()
-                point_0 = Point3D(start_point.X(), start_point.Y(), start_point.Z())
-                point_1 = Point3D(stop_point.X(), stop_point.Y(), stop_point.Z())
-
-                t = Tracklet(
-                    tracklet_id=tracklet_id,
-                    particle_id=particle_id,
-                    e_id=e_id,
-                    hits=hits
-                )
-                t.set_endpoints(point_0, point_1)
-                tracklets.append(t)
+                pattern_tracklet_ids.append(index)
 
             patterns_truth[pattern_idx] = pattern_tracklet_ids
 
